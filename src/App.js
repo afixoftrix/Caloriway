2a91dcd0f4a56603db9961bbe61bccf2cb8ae909
Unwana Essien <unwana.essien@outlook.com>
2019-08-20 11:35:24 +0100

    Setup testing environment

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Helmet } from 'react-helmet'
+import Map from "./components/Map";
+//<link href='https://api.mapbox.com/mapbox-gl-js/v1.2.0/mapbox-gl.css' rel='stylesheet' />
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    return (
+        <div>
+            <Helmet>
+                <link
+                    href="https://api.mapbox.com/mapbox-gl-js/v1.2.0/mapbox-gl.css"
+                    rel="stylesheet"
+                />
+            </Helmet>
+            <div>
+                App
+                <div id="map">
+                    <Map />
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default App;
